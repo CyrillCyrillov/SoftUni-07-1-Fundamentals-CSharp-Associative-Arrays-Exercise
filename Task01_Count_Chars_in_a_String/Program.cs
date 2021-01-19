e2efe4a0a32b1129b923d5bc1ba81dc46c1c1094
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-19 23:32:36 +0200

    Add files via upload

---
+++ Task01_Count_Chars_in_a_String/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task01_Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToCount = Console.ReadLine();
+
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            for (int i = 0; i <= stringToCount.Length - 1; i++)
+            {
+                char nextChar = stringToCount[i];
+                if(nextChar == ' ')
+                {
+                    continue;
+                }
+
+                if(charCount.ContainsKey(nextChar))
+                {
+                    charCount[nextChar]++;
+                }
+                else
+                {
+                    charCount.Add(nextChar, 1);
+                }
+            }
+
+            foreach (var item in charCount)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
