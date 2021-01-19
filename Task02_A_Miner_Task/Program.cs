23c1b8a546d8956b5eab35c5f4b61d341df0f833
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-19 23:33:21 +0200

    Add files via upload

---
+++ Task02_A_Miner_Task/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task02_A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> resoursColect = new Dictionary<string, double>();
+
+            while (true)
+            {
+                string nextResourse = Console.ReadLine();
+                
+                if(nextResourse.ToUpper() == "STOP")
+                {
+                    break;
+                }
+
+                double quantyty = double.Parse(Console.ReadLine());
+
+                if(resoursColect.ContainsKey(nextResourse))
+                {
+                    resoursColect[nextResourse] += quantyty;
+                }
+                else
+                {
+                    resoursColect.Add(nextResourse, quantyty);
+                }
+            }
+            
+            
+            foreach (var element in resoursColect)
+            {
+                Console.WriteLine($"{element.Key} -> {element.Value}");
+            }
+        }
+    }
+}
