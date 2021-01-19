1493170d40762350b2998e9fd4257130a7387ad8
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-19 23:37:01 +0200

    Add files via upload

---
+++ Task06A_Courses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taks06_Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
